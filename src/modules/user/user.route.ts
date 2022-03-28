4bf4a07ceb0aa69da6990a8d14fd676d7b5741d6
Daniel Klein <daniel.klein@develit.io>
2022-03-28 23:52:06 +0200

    userbyid

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,27 +1,41 @@
 import { FastifyInstance } from "fastify";
-import { getUsersHandler, loginHandler, registerUserHandler } from "./user.controller";
+import {
+  getUserByIdHandler,
+  getUsersHandler,
+  loginHandler,
+  registerUserHandler,
+} from "./user.controller";
 import { $ref } from "./user.schema";
 
 async function userRoutes(server: FastifyInstance) {
-    server.post('/', {
-        schema: {
-            body: $ref('createUserSchema'),
-            response: {
-                201: $ref('createUserResponseSchema')
-            }
-        }
-    }, registerUserHandler);
+  server.post(
+    "/",
+    {
+      schema: {
+        body: $ref("createUserSchema"),
+        response: {
+          201: $ref("createUserResponseSchema"),
+        },
+      },
+    },
+    registerUserHandler
+  );
 
-    server.post('/login', {
-        schema: {
-            body: $ref('loginSchema'),
-            response: {
-                200: $ref('loginResponseSchema')
-            }
-        }
-    }, loginHandler)
+  server.post(
+    "/login",
+    {
+      schema: {
+        body: $ref("loginSchema"),
+        response: {
+          200: $ref("loginResponseSchema"),
+        },
+      },
+    },
+    loginHandler
+  );
 
-    server.get('/', { preHandler: [server.authenticate] }, getUsersHandler);
+  server.get("/", { preHandler: [server.authenticate] }, getUsersHandler);
+  server.get("/:id", { preHandler: [server.authenticate] }, getUserByIdHandler);
 }
 
 export default userRoutes;
